a9b992f56746703ce8055457290079efa7c63fc8
Lucasgfln <163592473+Lucasgfln@users.noreply.github.com>
2024-03-27 18:44:27 -0300

    modified:   "Exercicios (UNP)/Lista de exercicio 2/Quest\303\243o_4_l2.java"

--- Exercicios (UNP)/Lista de exercicio 2/Questão_4_l2.java
+++ Exercicios (UNP)/Lista de exercicio 2/Questão_4_l2.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 public class Questão_4_l2 {
     public static void main(String[] args){
         Scanner ler = new Scanner(System.in);
-        float id = 0, pe = 0, s_id = 0, med_id = 0, p90 = 0, i = 1;
+        float id = 0, pe = 0, s_id = 0, med_id = 0;
+        int i = 1, p90 = 0;
         while (i<=7) {
             System.out.print("Digite a idade da "+ i +"ª pessoa: ");
-            id = ler.nextInt();
+            id = ler.nextFloat();
             System.out.print("Digite o peso da "+ i +"ª pessoa: ");
-            pe = ler.nextInt();
+            pe = ler.nextFloat();
             if (pe>90){
                 p90++;
             }
-            s_id = id + id;
+            s_id = s_id + id;
             i++;
         }
         med_id = s_id/7;
