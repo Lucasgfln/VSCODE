fb9bd0895eb1a75130a76857e0ff16a72c8f8663
Lucasgfln <163592473+Lucasgfln@users.noreply.github.com>
2024-04-02 07:29:40 -0300

    new file:   Projetos (UNP)/Vetor/Atvd3_2.java

---
+++ Projetos (UNP)/Vetor/Atvd3_2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Atvd3_2 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int n = 0, i, qNum_p =0, decisao;
+        System.out.print("Digite até que número você deseja verificar os pares: ");
+        decisao = ler.nextInt();
+        ler.close();
+        for (i = 0; i<=decisao; i++){
+            if (i % 2 == 0){
+                qNum_p++;
+            }
+        }
+        int [] núm_p = new int[qNum_p];
+        for (i = 0; i<=decisao; i++ ){
+            if(i % 2 == 0){
+                núm_p[n] = i;
+                n++;
+            }
+        }
+        for (n = 0; n < núm_p.length; n++){
+            System.out.println(núm_p[n]);
+        }
+        
+    }
+}
