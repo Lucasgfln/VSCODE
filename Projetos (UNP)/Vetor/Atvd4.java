32d6c1cfdc9334723028848352ae7bd19c4ece5e
Lucasgfln <163592473+Lucasgfln@users.noreply.github.com>
2024-04-03 16:45:51 -0300

    modified:   Projetos (UNP)/Vetor/Atvd4.java 	new file:   Projetos (UNP)/Vetor/Atvd5.java

--- Projetos (UNP)/Vetor/Atvd4.java
+++ Projetos (UNP)/Vetor/Atvd4.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class Atvd4{
     public static void main(String[] args){
-        
+        Scanner ler = new Scanner(System.in);
+        int [] num = new int[100];
+        int i, elmnt;
+        for (i = 0; i <= 100; i++){
+            
+        }
     }
 }
