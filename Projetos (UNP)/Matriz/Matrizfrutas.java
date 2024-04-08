a6054b86e70f0d8cdb99e984b50cf0df9d1cb1f2
Lucasgfln <163592473+Lucasgfln@users.noreply.github.com>
2024-04-08 09:26:48 -0300

    new file:   Projetos (UNP)/Matriz/Matrizfrutas.java

---
+++ Projetos (UNP)/Matriz/Matrizfrutas.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Matrizfrutas {
+    public static void main(String[] args) {
+        String [] [] frutas = new String[2][3];
+        Scanner ler = new Scanner(System.in);
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print("Digite uma fruta: ");
+                frutas [i] [j] = ler.nextLine();
+            }
+        }
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.println(frutas [i] [j]);
+                
+            }
+        }
+    }
+}
