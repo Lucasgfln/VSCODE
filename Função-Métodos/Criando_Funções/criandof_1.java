e4e36385c22dc78fdc766409005164d5c13f2da4
Lucasgfln <163592473+Lucasgfln@users.noreply.github.com>
2024-04-15 09:39:35 -0300

    new file:   "Fun\303\247\303\243o-M\303\251todos/Criando_Fun\303\247\303\265es/criandof_1.java"

---
+++ Função-Métodos/Criando_Funções/criandof_1.java
@@ -0,0 +1,11 @@
+public class criandof_1 {
+    public static void main(String[] args) {
+        int resultado = somar (num1:3, num2:5);
+        System.out.println(resultado);
+    }
+    public static int somar(int num1, int num2){
+        int soma = num1 + num2;
+        return soma;
+    }
+    
+}
