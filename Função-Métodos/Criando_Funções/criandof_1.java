b7732ac5310b279d7c028106525105f9d1818060
Lucasgfln <163592473+Lucasgfln@users.noreply.github.com>
2024-04-15 10:12:59 -0300

    modified:   "Fun\303\247\303\243o-M\303\251todos/Criando_Fun\303\247\303\265es/criandof_1.java"

--- Função-Métodos/Criando_Funções/criandof_1.java
+++ Função-Métodos/Criando_Funções/criandof_1.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class criandof_1 {
     public static void main(String[] args) {
-        int resultado = somar (3, 5);
+        Scanner ler = new Scanner(System.in);
+        int n1, n2, i;
+        System.out.print("Digite n1: ");
+        n1 = ler.nextInt();
+        System.out.print("Digite n2: ");
+        n2 = ler.nextInt();
+        int resultado = somar (n1, n2);
         System.out.println(resultado);
     }
     public static int somar(int num1, int num2){
