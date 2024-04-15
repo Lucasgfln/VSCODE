fa73ac05b508977dcb05c41c1c17b4fcf5c85d56
Lucasgfln <163592473+Lucasgfln@users.noreply.github.com>
2024-04-15 09:47:59 -0300

    modified:   "Fun\303\247\303\243o-M\303\251todos/Criando_Fun\303\247\303\265es/criandof_1.java"

--- Função-Métodos/Criando_Funções/criandof_1.java
+++ Função-Métodos/Criando_Funções/criandof_1.java
@@ -1,6 +1,6 @@
 public class criandof_1 {
     public static void main(String[] args) {
-        int resultado = somar (num1:3, num2:5);
+        int resultado = somar (3, 5);
         System.out.println(resultado);
     }
     public static int somar(int num1, int num2){
