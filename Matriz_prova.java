13429b0731c31b87d78e408e8b4d56eca8d33936
Lucasgfln <163592473+Lucasgfln@users.noreply.github.com>
2024-05-08 15:26:29 -0300

    new file:   Matriz_prova.java 	new file:   questao

---
+++ Matriz_prova.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Matriz_prova {
+    Scanner ler = new Scanner(System.in);
+    double [] [] matriz = new double[4][5];
+    double [] vetor = new double[5];
+    int i, j;
+    for (j = 0; j < 5; j++){
+
+        
+    }
+
+}
